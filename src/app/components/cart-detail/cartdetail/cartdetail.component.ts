5db0e2d38970a9436f57850d1af14c2a0556588d
David Elvis <daveelvis.369@gmail.com>
2025-03-12 12:06:06 -0700

    update

--- src/app/components/cart-detail/cartdetail/cartdetail.component.ts
+++ src/app/components/cart-detail/cartdetail/cartdetail.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Cart } from '../../../Entities/Cart/cart';
+import { CartService } from '../../../Services/CartService/cart.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cartdetail',
-  imports: [],
+  imports: [NgFor, CommonModule],
   templateUrl: './cartdetail.component.html',
   styleUrl: './cartdetail.component.css'
 })
-export class CartdetailComponent {
-constructor(private router:ActivatedRoute){
-  
+export class CartdetailComponent implements OnInit{
+
+  public cartItems:Cart[]=[];
+  public totalPrice:number=0;
+  public itemQuantity:number=0;
+
+constructor(private router:ActivatedRoute, private service:CartService){
+
 }
+
+ngOnInit(): void {
+  this.loadTotal();
+}
+
+loadTotal():void{
+this.service.totalPrice.subscribe(data=>{
+  this.totalPrice=data;
+});
+
+this.itemQuantity=this.cartItems.length;
+
+this.cartItems=this.service.books;
+}
+
 }
